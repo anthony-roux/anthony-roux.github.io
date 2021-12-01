a9cd1af104aeac84f79a36b6ce2fd9b5a2de28fd
anthony.roux <roux.anthony@hetic.net>
2021-12-01 12:59:29 +0100

    initialised docker psr-4

---
+++ cms-php/app/src/Test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+class Test 
+{
+  public static function test() {
+    return 'bonjour';
+  }
+}
---
+++ cms-php/app/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '87864a0d3b1322dbe274f86e2af65679d369311c',
+    'name' => 'anthony.roux/app',
+  ),
+  'versions' => 
+  array (
+    'anthony.roux/app' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '87864a0d3b1322dbe274f86e2af65679d369311c',
+    ),
+  ),
+);
